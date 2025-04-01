104a94971c03fb4c4564847bdb67be97f3a1a815
sobhi69 <mhegazy6371@gmail.com>
2025-04-01 11:34:08 +0200

    clean up

--- frontend/src/routes/Router.tsx
+++ frontend/src/routes/Router.tsx
@@ -1,6 +1,5 @@
 import { FC } from 'react'
 import { Route, Routes } from 'react-router-dom';
-import Home from '../pages/home/Home';
 import EasyToEarn from '../pages/easyToEarn/EasyToEarn';
 import Profile from '../pages/profile/Profile';
 import NotFound from '../pages/notFound/NotFound';
@@ -12,7 +11,6 @@ interface RouterProps {
 const Router: FC<RouterProps> = ({  }) => {
   return (
     <Routes>
-        {/* <Route path='/' element={<Home />} /> */}
         <Route path='/' element={<EasyToEarn />}/>
         <Route path='/profile' element={<Profile />}/>
         <Route path='*' element={<NotFound />}/>
--- jj.ts
+++
@@ -1,5 +0,0 @@
-// in the name of God the most gracios the most mercifull 
-// hey dud 
-// i know that you're going through a good feeling 
-// what do i do 
-// 
